e3234d3ffcc2b39311cb6a60738eb0bb54e1d609
Erlikb <angelitaee0@gmail.com>
2023-12-12 08:40:10 -0500

    act

---
+++ trabajo.py
@@ -0,0 +1,37 @@
+# ejercicio 1
+
+num1 = int(input("Ingresa un numero: "))
+num2 = int(input("Ingresa el segundo numero: "))
+
+suma = num1 + num2 #operaciones aritmeticas basicas
+resta = num1 - num2
+multiplicacion = num1 * num2
+division = num1 / num2
+
+print("La sumam de los dos numeros es: ",suma,
+      "\nLa resta de los dos numeros es: ",resta,
+      "\nLa multiplicacion de los dos numeros es: ",multiplicacion,
+      "\nLa division de los dos numeros es: ",division) # impresion de los resultados
+
+# Ejercicio numero 2
+
+var1 = input("Ingresa tu nombre: ") # nombre que ingresa el usuario
+
+long = len(var1) # variable para saber cuantas letras tiene el nombre del usuario
+
+print("Tu nombre completo es: ",var1,
+      "\nLa longitud de tu nombre es: ",long) # impresion de los resultados
+
+# Ejercicio numero 3
+
+ent = int(input("Ingresa un numero entero: ")) # variables que ingresa el usuario
+dec = float(input("Ingresa un numero con dos decimales: "))
+
+entc = float(ent) # transformacion de los numeros ingresados
+dece = int(dec)
+
+print("El numero entero que ingresaste es: ",ent,
+      "\nEl numero decimal que ingresate es: ",dec,
+      "\nEl numero entero transformado a decimal es: ",entc,
+      "\nEl numero decimal transformado a entero es: ",dece) # impresion de los resultados
+
